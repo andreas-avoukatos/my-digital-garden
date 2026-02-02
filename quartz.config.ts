800fa56bce57332413ec5514228f06cf3eb81d94
Andreas Avoukatos <aavoukatos@di.uoa.gr>
2026-02-02 11:18:36 +0200

    Quartz sync: Feb 2, 2026, 11:18 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -72,6 +72,7 @@ const config: QuartzConfig = {
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
+      Plugin.ContentIndex(),
     ],
     filters: [Plugin.RemoveDrafts()],
     emitters: [
