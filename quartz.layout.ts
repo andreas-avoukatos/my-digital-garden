94710d5d9d1c5d00d9789a69e9b8ff5097832ace
Andreas Avoukatos <aavoukatos@di.uoa.gr>
2026-02-07 13:16:14 +0200

    Quartz sync: Feb 7, 2026, 1:16 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -18,9 +18,7 @@ export const sharedPageComponents: SharedLayout = {
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
     Component.ConditionalRender({
-      component: Component.Breadcrumbs({
-      spacerSymbol: "⊢",
-   }),
+      component: Component.Breadcrumbs(),
       condition: (page) => page.fileData.slug !== "index",
     }),
     Component.ArticleTitle(),
