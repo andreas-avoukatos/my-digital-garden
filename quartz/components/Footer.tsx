7355b3ade8cc60134336355f8aec6c7eb87140ba
Andreas Avoukatos <aavoukatos@di.uoa.gr>
2026-08-06 09:33:06 +0300

    Quartz sync: Aug 6, 2026, 9:33 AM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -12,20 +12,37 @@ export default ((opts?: Options) => {
     const year = new Date().getFullYear()
     const links = opts?.links ?? []
     return (
-      <footer class={`${displayClass ?? ""}`}>
-        <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
-        </p>
-        <ul>
-          {Object.entries(links).map(([text, link]) => (
-            <li>
-              <a href={link}>{text}</a>
-            </li>
-          ))}
-        </ul>
-      </footer>
-    )
+  <footer class={`${displayClass ?? ""}`}>
+    <hr />
+    <p>
+      {i18n(cfg.locale).components.footer.createdWith}{" "}
+      <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+    </p>
+    <ul>
+      {Object.entries(links).map(([text, link]) => (
+        <li>
+          <a href={link}>{text}</a>
+        </li>
+      ))}
+    </ul>
+    
+    {/* GoatCounter View Stats */}
+    <div id="gc-stats"></div>
+    <script dangerouslySetInnerHTML={{
+      __html: `
+        var t = setInterval(function() {
+          if (window.goatcounter && window.goatcounter.visit_count) {
+            clearInterval(t)
+            window.goatcounter.visit_count({
+              append: '#gc-stats',
+              no_branding: true
+            }) 
+          }
+        }, 100)
+      `
+    }}></script>
+  </footer>
+  )
   }
 
   Footer.css = style
